8a2d55b7571a7d2efe286d82fbef060512f831a5
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2014-08-11 22:10:01 +0300

    Don't filter by PID needlessly in huion_raw_event

--- hid-huion.c
+++ hid-huion.c
@@ -231,16 +231,12 @@ static int huion_raw_event(struct hid_device *hdev, struct hid_report *report,
 {
 	struct usb_interface *intf = to_usb_interface(hdev->dev.parent);
 
-	switch (hdev->product) {
-	case USB_DEVICE_ID_HUION_TABLET:
-		/* If this is a pen input report */
-		if (intf->cur_altsetting->desc.bInterfaceNumber == 0 &&
-		    report->type == HID_INPUT_REPORT &&
-		    report->id == 0x07 && size >= 2)
-			/* Invert the in-range bit */
-			data[1] ^= 0x40;
-		break;
-	}
+	/* If this is a pen input report */
+	if (intf->cur_altsetting->desc.bInterfaceNumber == 0 &&
+	    report->type == HID_INPUT_REPORT &&
+	    report->id == 0x07 && size >= 2)
+		/* Invert the in-range bit */
+		data[1] ^= 0x40;
 
 	return 0;
 }
