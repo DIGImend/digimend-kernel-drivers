692a05c1fd00944d4e54371f0f397be368e93a1e
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2014-08-18 17:49:24 +0300

    Enable Yiynova tablets also
    Invoke huion_tablet_enable for Yiynova tablets also.
    This fixes Yiynova tablet enabling.

--- hid-huion.c
+++ hid-huion.c
@@ -224,6 +224,7 @@ static int huion_probe(struct hid_device *hdev, const struct hid_device_id *id)
 
 	switch (id->product) {
 	case USB_DEVICE_ID_HUION_TABLET:
+	case USB_DEVICE_ID_YIYNOVA_MXP:
 		/* If this is the pen interface */
 		if (intf->cur_altsetting->desc.bInterfaceNumber == 0) {
 			rc = huion_tablet_enable(hdev);
