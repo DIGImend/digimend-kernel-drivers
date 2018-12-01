01df2331f1864d5e4fb55de79a21d9616ba3e973
Nikolai Kondrashov <spbnick@gmail.com>
2018-12-01 20:49:04 +0200

    uclogic: Support Ugee M708 v2

--- hid-uclogic-params.c
+++ hid-uclogic-params.c
@@ -1012,6 +1012,8 @@ int uclogic_params_init(struct uclogic_params *params,
 		     USB_DEVICE_ID_UCLOGIC_UGEE_TABLET_45):
 	case VID_PID(USB_VENDOR_ID_UCLOGIC,
 		     USB_DEVICE_ID_UCLOGIC_UGEE_TABLET_47):
+	case VID_PID(USB_VENDOR_ID_UGEE,
+		     USB_DEVICE_ID_UGEE_TABLET_M708_V2):
 		rc = uclogic_params_huion_init(&p, hdev);
 		if (rc != 0) {
 			goto cleanup;
