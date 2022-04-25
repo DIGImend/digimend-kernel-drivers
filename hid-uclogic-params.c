59c72ae1612355d1db39cb68f457313c953eb21e
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2022-04-25 15:42:49 +0200

    uclogic: typo FALL THROUGH
    gcc expects /* FALLTHROUGH */
    cf. https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html
    
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- hid-uclogic-params.c
+++ hid-uclogic-params.c
@@ -1363,7 +1363,7 @@ int uclogic_params_init(struct uclogic_params *params,
 			}
 			break;
 		}
-		/* FALL THROUGH */
+		/* FALLTHROUGH */
 	case VID_PID(USB_VENDOR_ID_HUION,
 		     USB_DEVICE_ID_HUION_TABLET):
 	case VID_PID(USB_VENDOR_ID_HUION,
