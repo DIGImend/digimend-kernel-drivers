ab99fe8e412466d8f277b9c20660fb76c6982bc8
José Expósito <jose.exposito89@gmail.com>
2022-06-11 13:39:11 +0200

    HID: uclogic: Make template placeholder IDs generic
    Up until now, the report descriptor template parameter IDs were only
    used with pen report descriptors and they were named accordingly.
    
    Rename the enum and the total number of IDs to make them interface
    agnostic.
    
    Refactor, no functional changes.
    
    Signed-off-by: José Expósito <jose.exposito89@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- hid-uclogic-params.c
+++ hid-uclogic-params.c
@@ -236,7 +236,7 @@ static int uclogic_params_pen_init_v1(struct uclogic_params_pen *pen,
 	const int len = 12;
 	s32 resolution;
 	/* Pen report descriptor template parameters */
-	s32 desc_params[UCLOGIC_RDESC_PEN_PH_ID_NUM];
+	s32 desc_params[UCLOGIC_RDESC_PH_ID_NUM];
 	__u8 *desc_ptr = NULL;
 
 	/* Check arguments */
@@ -381,7 +381,7 @@ static int uclogic_params_pen_init_v2(struct uclogic_params_pen *pen,
 	size_t i;
 	s32 resolution;
 	/* Pen report descriptor template parameters */
-	s32 desc_params[UCLOGIC_RDESC_PEN_PH_ID_NUM];
+	s32 desc_params[UCLOGIC_RDESC_PH_ID_NUM];
 	__u8 *desc_ptr = NULL;
 
 	/* Check arguments */
